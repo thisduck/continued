8a521f49dc464a314d6b9b860afafaca8cd6d923
Adnan Ali <adnan.ali@gmail.com>
2013-04-01 13:11:16 -0400

    initial commit with mongomapper.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Continue::Application.config.secret_token = 'd1f892125ef0b1da37b7275d962245ffdcc456f921fddc565fec2a083922910bd9b3252538983feac40596a4b9832767ea8956364e0c2e86a410d3f2d8a4fa6c'
